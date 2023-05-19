9546e5676826c863bd2db033b00ce0f5f65fcb9f
tejasrana95 <tejasrana95@gmail.com>
2023-05-19 15:52:45 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log('Hello!');
+
+console.log('Hello!');
+
+function validateForm() {
+  const form = document.querySelectorAll('.input');
+  form.forEach((data) => {
+    if (data.className.includes('required')) {
+      if (data.value.trim() === '') {
+        document.querySelectorAll('.error.' + data.name)[0].style.display =
+          'block';
+      } else {
+        document.querySelectorAll('.error.' + data.name)[0].style.display =
+          'none';
+      }
+    }
+    if (data.className.includes('min6Char')) {
+      if (data.value.length <= 6) {
+        document.querySelectorAll(
+          '.error.' + data.name + '.min6Char'
+        )[0].style.display = 'block';
+      } else {
+        document.querySelectorAll(
+          '.error.' + data.name + '.min6Char'
+        )[0].style.display = 'none';
+      }
+    }
+  });
+}
