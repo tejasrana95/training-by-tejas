fffa43a86284a8a65619dd0f971057c4bfd0fa2b
tejasrana95 <tejasrana95@gmail.com>
2023-05-24 15:25:48 +0530

    1 file modified, 1 added

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 console.log('Hello!');
 
-console.log('Hello!');
-
 function validateForm() {
   const form = document.querySelectorAll('.input');
   form.forEach((data) => {
@@ -26,4 +24,34 @@ function validateForm() {
       }
     }
   });
+}
+
+function callApi(filter) {
+  fetch('https://mocki.io/v1/40d0bc00-7c09-41b9-8ee2-a028ef749cee')
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      if (data.length > 0) {
+        let tableTd = '';
+        console.log(data);
+        data.forEach(tableData => {
+            if(typeof filter !== null && filter === true){
+                if(!tableData.isActive) {return};
+            }
+           
+            tableTd = tableTd+'<tr><td>'+tableData.id+'</td><td>'+tableData.name+'</td><td>'+tableData.gender+'</td><td>'+tableData.email+'</td><td>'+tableData.age+'</td><td>'+tableData.phone+'</td><td><img src="'+tableData.picture+'" /></td><td>'+tableData.friends.length+'</td></tr>'
+        });
+        document.querySelector('#tableData').innerHTML = tableTd;
+      }
+     
+    });
+}
+callApi(null);
+
+
+function updateRecords(e){
+    console.log(e);
+    const checked = e.target.checked;
+    callApi(checked);
 }
